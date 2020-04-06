b2b3e92cd66890d046120a9a06d670c3e3f01a9e
Crauseo22 <62118430+Crauseo22@users.noreply.github.com>
2020-04-05 21:05:51 -0300

    commit 1, definicion de clase usuario

---
+++ SCM/Usuario.java
@@ -0,0 +1,11 @@
+package SCM;
+//Definimos clase usuarios
+
+public class Usuario{
+    private String nombre; 
+    private String cedula; 
+    private String contrasena; 
+    private int edad; 
+    private int cantHijos; 
+    private double saldo; 
+}
